0f4e65dfb0de901c47f53def8c6e95982c0c4729
byaskew3 <byaskew3@gmail.com>
2022-02-16 16:50:37 -0700

    started JS

---
+++ app.js
@@ -0,0 +1,16 @@
+// cache DOM
+let inputEl = document.querySelector('.input-el')
+
+
+// create function length(), volume(), mass()
+function length(num) {
+    let lengthValue = num *= 3.281;
+};
+length(5);
+function volume(num) {
+    let volumeValue = num *= 0.264;
+};
+
+function mass(num) {
+    let massValue = num *= 2.205;
+};
