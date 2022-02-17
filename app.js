cb06091b0218124072e48d5083fd4842a8e4a67b
byaskew3 <byaskew3@gmail.com>
2022-02-16 17:49:16 -0700

    js(in progress)

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
 // cache DOM
 let inputEl = document.querySelector('.input-el');
-let lengthEl = document.querySelector('.length-span');
+let lengthEl = document.querySelectorAll('.length-span');
 let lengthMeasure = document.querySelector('.length-measure')
-let volumeEl = document.querySelector('.volume-span');
+let volumeEl = document.querySelectorAll('.volume-span');
 let volumeMeasure = document.querySelector('.volume-measure')
-let massEl = document.querySelector('.mass-span');
+let massEl = document.querySelectorAll('.mass-span');
 let massMeasure = document.querySelector('.mass-measure')
 
+console.log(lengthEl);
 // create function length(), volume(), mass()
 function length(num) {
-    lengthEl.textContent = num;
+    for(let i = 0; i < lengthEl.length; i++) {
+        lengthEl[i].textContent = num;
+    }
     let lengthValue = num *= 3.281;
     lengthMeasure.textContent = lengthValue;
 };
@@ -17,13 +20,17 @@ function length(num) {
 
 
 function volume(num) {
-    volumeEl.textContent = num;
+    for(let i = 0; i < volumeEl.length; i++) {
+        volumeEl[i].textContent = num;
+    }
     let volumeValue = num *= 0.264;
     volumeMeasure.textContent = volumeValue;
 };
 
 function mass(num) {
-    massEl.textContent = num;
+    for(let i = 0; i < massEl.length; i++){
+        massEl[i].textContent = num;
+    }
     let massValue = num *= 2.205;
     massMeasure.textContent = massValue;
 };
